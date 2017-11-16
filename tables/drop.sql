486e0b90417a75cfd283e948a61294ce8f93356b
Sturjak <sturgeonjake52@gmail.com>
2017-11-16 15:57:05 +0000

    sql stuff

---
+++ tables/drop.sql
@@ -0,0 +1,10 @@
+PRAGMA foreign_keys = 0;
+
+DROP TABLE Patient;
+DROP TABLE Address;
+DROP TABLE Subscription;
+DROP TABLE HealthCarePlan;
+DROP TABLE Appointment;
+DROP TABLE Partner;
+DROP TABLE Record;
+DROP TABLE Treatment;
