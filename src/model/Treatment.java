fbd18aa757c0a23f4ed1c793d0bc620afcd58b29
Jia Hua Ng <Acb16jhn@shefuniad.shef.ac.uk>
2017-11-17 22:34:46 +0000

    added treatment model

---
+++ src/model/Treatment.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Treatment {
+	private String name;
+	private String type;
+	private int cost;
+	
+	public Treatment(String name, String type, int cost) {
+		this.name = name;
+		this.type = type;
+		this.cost= cost;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getCost() {
+		return cost;
+	}
+
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+}
