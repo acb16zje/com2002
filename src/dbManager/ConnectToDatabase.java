e8d38ca1783985486bbf90d24d4ebd6f40326446
Sturjak <sturgeonjake52@gmail.com>
2017-11-17 16:32:24 +0000

    added dbConnection class

---
+++ src/dbManager/ConnectToDatabase.java
@@ -0,0 +1,40 @@
+package dbManager;
+
+import java.sql.*;
+
+/**
+ * @author Jake Sturgeon
+ * @version 1.0 on 17/11/2017
+ */
+public class ConnectToDatabase {
+
+    Connection con;  //Connect to db
+    public ConnectToDatabase() {
+        con = null;
+        connect();
+    }
+
+    private void connect() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team006", "team006", "72b1d11b");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("closed");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ConnectToDatabase c = new ConnectToDatabase();
+
+    }
+
+}
