0151922e33599bae4011a33c4ef88a641b15e913
Ruairi Sinkler <sinky_son@hotmail.co.uk>
2017-11-18 14:55:13 +0000

    HealthCarePlan, Partner and Subscription classes created

---
+++ src/dbManager/HealthCarePlan.java
@@ -0,0 +1,39 @@
+package dbManager;
+
+public class HealthCarePlan {
+
+	private String name;
+	private int monthlyFee;
+	private int checkUps;
+	private int hygieneVisits;
+	private int repairWorks;
+
+	public HealthCarePlan(String name, int monthlyFee, int checkUps, int hygieneVisits, int repairWorks) {
+		this.name = name;
+		this.monthlyFee = monthlyFee;
+		this.checkUps = checkUps;
+		this.hygieneVisits = hygieneVisits;
+		this.repairWorks = repairWorks;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getMonthlyFee() {
+		return monthlyFee;
+	}
+
+	public int getCheckUps() {
+		return checkUps;
+	}
+
+	public int getHygieneVisits() {
+		return hygieneVisits;
+	}
+
+	public int getRepairWorks() {
+		return repairWorks;
+	}
+	
+}
