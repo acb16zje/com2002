ceee3cd8097a8938adbdc274aa6c26e235c4d171
Jia Hua Ng <Acb16jhn@shefuniad.shef.ac.uk>
2017-11-20 23:28:48 +0000

    added book holiday ui

--- src/view/BookAppointment.java
+++ src/view/BookAppointment.java
@@ -18,6 +18,7 @@ import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 public class BookAppointment extends JDialog {
+	private JTextField patientID;
 
     /**
      * Create the dialog.
@@ -35,18 +36,43 @@ public class BookAppointment extends JDialog {
         mainLabel.setBounds(110, 23, 165, 19);
         mainLabel.setFont(new Font("Dialog", Font.BOLD, 16));
         contentPanel.add(mainLabel);
-
+        patientID = new JTextField();
+        patientID.setFont(new Font("Dialog", Font.PLAIN, 16));
+        patientID.setColumns(10);
+        patientID.setBounds(105, 88, 194, 23);
+        contentPanel.add(patientID);
+        
+        JLabel lblPatientID = new JLabel("Patient ID:");
+        lblPatientID.setBounds(24, 95, 75, 15);
+        contentPanel.add(lblPatientID);
+        
+        JLabel lblType = new JLabel("Type:");
+        lblType.setBounds(24, 57, 39, 15);
+        contentPanel.add(lblType);
+        
+        JRadioButton checkUpRadioButton = new JRadioButton(radioButton);
+        checkUpRadioButton.setBounds(105, 53, 90, 23);
+        contentPanel.add(checkUpRadioButton);
+        
+        JRadioButton treatmentRadioButton = new JRadioButton("Treatment");
+        treatmentRadioButton.setBounds(197, 53, 76, 23);
+        contentPanel.add(treatmentRadioButton);
+        
+        JRadioButton holidayRadioButton = new JRadioButton("Holiday");
+        holidayRadioButton.setBounds(289, 53, 76, 23);
+        contentPanel.add(holidayRadioButton);
+        
         // Date label
-        JLabel dateLabel = new JLabel("Date:");
-        dateLabel.setBounds(42, 74, 39, 15);
-        contentPanel.add(dateLabel);
+        JLabel lblDate = new JLabel("Date:");
+        lblDate.setBounds(34, 135, 39, 15);
+        contentPanel.add(lblDate);
 
         //calendar object to create date combobox
         Calendar tempCal = new GregorianCalendar();
 
         // ComboBox for day
         JComboBox comboDay = new JComboBox();
-        comboDay.setBounds(123, 69, 50, 24);
+        comboDay.setBounds(115, 130, 50, 24);
         contentPanel.add(comboDay);
         int today = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
         for (int i = 1; i <= tempCal.getActualMaximum(Calendar.DAY_OF_MONTH); i++) {
@@ -56,7 +82,7 @@ public class BookAppointment extends JDialog {
 
         // ComboBox for month
         JComboBox comboMonth = new JComboBox();
-        comboMonth.setBounds(185, 69, 50, 24);
+        comboMonth.setBounds(177, 130, 50, 24);
         contentPanel.add(comboMonth);
         for (int i = 1; i <= 12; i++) {
             comboMonth.addItem(i);
@@ -65,7 +91,7 @@ public class BookAppointment extends JDialog {
 
         // ComboBox for year
         JComboBox comboYear = new JComboBox();
-        comboYear.setBounds(247, 69, 76, 24);
+        comboYear.setBounds(239, 130, 76, 24);
         contentPanel.add(comboYear);
         int currentYear = Calendar.getInstance().get(Calendar.YEAR);
         for (int i = currentYear; i <= currentYear + 2; i++) {
@@ -76,13 +102,13 @@ public class BookAppointment extends JDialog {
         comboMonth.addActionListener(new DateListener(comboDay, comboMonth, comboYear));
         comboYear.addActionListener(new DateListener(comboDay, comboMonth, comboYear));
         // Label for Start Time
-        JLabel startTimeLabel = new JLabel("Start time:");
-        startTimeLabel.setBounds(42, 114, 76, 15);
-        contentPanel.add(startTimeLabel);
+        JLabel lblStartTime = new JLabel("Start Time:");
+        lblStartTime.setBounds(34, 175, 76, 15);
+        contentPanel.add(lblStartTime);
 
         // ComboBox for start time, 20 minutes interval
         JComboBox comboStartTime = new JComboBox();
-        comboStartTime.setBounds(123, 109, 68, 24);
+        comboStartTime.setBounds(115, 170, 68, 24);
         comboStartTime.setEditable(false);
         for (int hour = 9; hour < 17; hour++) {
             for (int min = 0; min < 6; min += 2) {
@@ -95,38 +121,124 @@ public class BookAppointment extends JDialog {
             }
         }
         contentPanel.add(comboStartTime);
-
-        // Label for type
-        JLabel typeLabel = new JLabel("Type:");
-        typeLabel.setBounds(42, 154, 39, 15);
-        contentPanel.add(typeLabel);
-
-        // Label for Check-up / hygiene
-        JRadioButton checkUpRadioButton = new JRadioButton(radioButton);
-        checkUpRadioButton.setBounds(123, 150, 90, 23);
-        contentPanel.add(checkUpRadioButton);
-
-        // Label for treatment
-        JRadioButton treatmentRadioButton = new JRadioButton("Treatment");
-        treatmentRadioButton.setBounds(218, 150, 99, 23);
-        contentPanel.add(treatmentRadioButton);
-
+       
+        JLabel lblEndDate = new JLabel("End Date:");
+        lblEndDate.setBounds(34, 215, 50, 15);
+        lblEndDate.setVisible(false);
+        lblEndDate.setEnabled(false);
+        contentPanel.add(lblEndDate);
+        
+        JComboBox comboEndDay = new JComboBox();
+        comboEndDay.setBounds(115, 210, 50, 24);
+        comboEndDay.setVisible(false);
+        comboEndDay.setEnabled(false);
+        for (int i = 1; i <= tempCal.getActualMaximum(Calendar.DAY_OF_MONTH); i++) {
+            comboEndDay.addItem(i);
+        }
+        comboEndDay.setSelectedItem(tempCal.get(Calendar.DAY_OF_MONTH));
+        contentPanel.add(comboEndDay);
+        
+        JComboBox comboEndMonth = new JComboBox();
+        comboEndMonth.setBounds(177, 210, 50, 24);
+        comboEndMonth.setVisible(false);
+        comboEndMonth.setEnabled(false);
+        for (int i = 1; i <= 12; i++) {
+            comboEndMonth.addItem(i);
+        }
+        comboEndMonth.setSelectedItem(tempCal.get(Calendar.MONTH) + 1);
+        contentPanel.add(comboEndMonth);
+        
+        JComboBox comboEndYear = new JComboBox();
+        comboEndYear.setBounds(239, 210, 76, 24);
+        comboEndYear.setVisible(false);
+        comboEndYear.setEnabled(false);
+        int currentEndYear = Calendar.getInstance().get(Calendar.YEAR);
+        for (int i = currentEndYear; i <= currentYear + 2; i++) {
+            comboEndYear.addItem(i);
+        }
+        comboYear.setSelectedItem(tempCal.get(Calendar.YEAR));
+        contentPanel.add(comboEndYear);
+        
+        JLabel lblEndTime = new JLabel("End Time:");
+        lblEndTime.setBounds(34, 254, 76, 15);
+        lblEndTime.setVisible(false);
+        lblEndTime.setEnabled(false);
+        contentPanel.add(lblEndTime);
+        
+        JComboBox comboEndTime = new JComboBox();
+        comboEndTime.setEditable(false);
+        comboEndTime.setBounds(115, 249, 68, 24);
+        comboEndTime.setVisible(false);
+        comboEndTime.setEnabled(false);
+        for (int hour = 9; hour <= 17; hour++) {
+        	if (hour == 9) {
+        		for (int min = 2; min < 6; min += 2) {
+        			comboEndTime.addItem("0" + String.valueOf(hour) + ":" + String.valueOf(min) + "0");
+        		}
+        	}
+        	else if (hour == 17 ) {
+        		comboEndTime.addItem("17:00");
+        	}
+        	else {
+	            for (int min = 0; min < 6; min += 2) {
+	                    comboEndTime.addItem(String.valueOf(hour) + ":" + String.valueOf(min) + "0");
+	                }
+	        }
+        }
+           
+        contentPanel.add(comboEndTime);
+        
+        comboEndMonth.addActionListener(new DateListener(comboEndDay, comboEndMonth, comboEndYear));
+        comboEndYear.addActionListener(new DateListener(comboEndDay, comboEndMonth, comboEndYear));
+        
+        holidayRadioButton.addActionListener(e ->{
+        	lblEndDate.setVisible(true);
+        	lblEndDate.setEnabled(true);
+        	comboEndDay.setVisible(true);
+        	comboEndDay.setEnabled(true);
+        	comboEndMonth.setVisible(true);
+        	comboEndMonth.setEnabled(true);
+        	comboEndYear.setVisible(true);
+        	comboEndYear.setEnabled(true);
+        	lblEndTime.setVisible(true);
+        	lblEndTime.setEnabled(true);
+        	comboEndTime.setVisible(true);
+        	comboEndTime.setEnabled(true);
+        });
+        treatmentRadioButton.addActionListener(e ->{
+        	lblEndDate.setVisible(false);
+        	lblEndDate.setEnabled(false);
+        	comboEndDay.setVisible(false);
+        	comboEndDay.setEnabled(false);
+        	comboEndMonth.setVisible(false);
+        	comboEndMonth.setEnabled(false);
+        	comboEndYear.setVisible(false);
+        	comboEndYear.setEnabled(false);
+        	lblEndTime.setVisible(false);
+        	lblEndTime.setEnabled(false);
+        	comboEndTime.setVisible(false);
+        	comboEndTime.setEnabled(false);
+        });
+        checkUpRadioButton.addActionListener(e ->{
+        	lblEndDate.setVisible(false);
+        	lblEndDate.setEnabled(false);
+        	comboEndDay.setVisible(false);
+        	comboEndDay.setEnabled(false);
+        	comboEndMonth.setVisible(false);
+        	comboEndMonth.setEnabled(false);
+        	comboEndYear.setVisible(false);
+        	comboEndYear.setEnabled(false);
+        	lblEndTime.setVisible(false);
+        	lblEndTime.setEnabled(false);
+        	comboEndTime.setVisible(false);
+        	comboEndTime.setEnabled(false);
+        });
+        
         ButtonGroup treatmentGroup = new ButtonGroup();
         treatmentGroup.add(checkUpRadioButton);
         treatmentGroup.add(treatmentRadioButton);
-
-        // Label for patient ID
-        JLabel patientIDLabel = new JLabel("Patient ID:");
-        patientIDLabel.setBounds(42, 194, 75, 15);
-        contentPanel.add(patientIDLabel);
-
-        // Text field for patient ID input
-        JTextField patientIDTextField = new JTextField();
-        patientIDTextField.setBounds(123, 190, 194, 23);
-        patientIDTextField.setFont(new Font("Dialog", Font.PLAIN, 16));
-        contentPanel.add(patientIDTextField);
-        patientIDTextField.setColumns(10);
-
+        treatmentGroup.add(holidayRadioButton);
+        
         // Button panel
         JPanel buttonPane = new JPanel();
         buttonPane.setLayout(new FlowLayout(FlowLayout.CENTER));
@@ -162,7 +274,7 @@ public class BookAppointment extends JDialog {
 
         // Basic settings
         setTitle("Book Appointment");
-        setBounds(100, 100, 395, 306);
+        setBounds(100, 100, 395, 406);
         setResizable(false);
     }
 
