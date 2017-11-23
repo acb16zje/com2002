ec187290e34e7fd08275d206f58d68b9f9f6af39
Ruairi Sinkler <sinky_son@hotmail.co.uk>
2017-11-23 16:09:46 +0000

    MoneyFilter works with money values now

--- tables/tables.sql
+++ tables/tables.sql
@@ -27,7 +27,7 @@ CREATE TABLE Partner (
 
 CREATE TABLE HealthCarePlan (
   planName     VARCHAR(30)    NOT NULL,
-  monthlyFee   DECIMAL(5, 2)  NOT NULL,
+  monthlyFee   DECIMAL(10, 2)  NOT NULL,
   checkUp      INT            NOT NULL,
   hygieneVisit INT            NOT NULL,
   repairWork   INT            NOT NULL,
@@ -61,7 +61,7 @@ CREATE TABLE Appointment (
 CREATE TABLE Treatment (
   name VARCHAR(30)    NOT NULL,
   type VARCHAR(20)    NOT NULL,
-  cost DECIMAL(5, 2)  NOT NULL,
+  cost DECIMAL(10, 2)  NOT NULL,
   PRIMARY KEY (name)
 );
 
